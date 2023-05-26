8f454aafaa6fb062723c2971c15255f24b3bc5e6
sonadro <sondre.rooth@gmail.com>
2023-05-26 09:50:18 +0200

    Middleware for checking if users are logged in

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -4,11 +4,23 @@ module.exports.home_get = (req, res) => {
 };
 
 module.exports.signin_get = (req, res) => {
-    res.render('signin', { title: 'Sign in', navText: '' });
+    // check if user is logged in
+    if (!res.locals.loggedIn) {
+        res.render('signin', { title: 'Sign in', navText: '' });
+    } else {
+        // user is logged in, redirect to front page
+        res.redirect('/');
+    };
 };
 
 module.exports.signup_get = (req, res) => {
-    res.render('signup', { title: 'Sign up', navText: '' });
+    // check if user is logged in
+    if (!res.locals.loggedIn) {
+        res.render('signup', { title: 'Sign up', navText: '' });
+    } else {
+        // user is logged in, redirect to front page
+        res.redirect('/');
+    };
 };
 
 module.exports.userpage_get = (req, res) => {
@@ -17,6 +29,17 @@ module.exports.userpage_get = (req, res) => {
 };
 
 module.exports.userhome_get = (req, res) => {
-    const username = req.params.username;
-    res.render('userhome', { title: username, navText: username });
+    // check if user is logged in
+    if (res.locals.loggedIn) {
+        const username = req.params.username;
+        if (username === undefined) {
+            // if username is undefined, it means the user went to /home, redirect them to /home/theirUsername
+            res.redirect(`/home/${res.locals.loggedInUser}`);
+        } else {
+            res.render('userhome', { title: username, navText: username });
+        };
+    } else {
+        // user isn't logged in, redirect to login page
+        res.redirect('/sign-in');
+    };
 };
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,14 +1,19 @@
 // imports
 const { Router } = require('express');
 const mainController = require('../controllers/mainController');
+const { loggedInCheck } = require('../middleware/auth');
 
 const router = Router();
+
+// run logged in check for every page:
+router.use(loggedInCheck);
 
 // routes
 router.get('/', mainController.home_get);
 router.get('/sign-in', mainController.signin_get);
 router.get('/sign-up', mainController.signup_get);
 router.get('/user/:username', mainController.userpage_get);
+router.get('/home', mainController.userhome_get);
 router.get('/home/:username', mainController.userhome_get);
 
 module.exports = router;
